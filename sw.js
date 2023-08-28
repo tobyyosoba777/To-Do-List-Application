993113cc0650fbc76852748b71f847aa42297a0c
tobiloba <tobiosoba777@gmail.com>
2023-08-28 14:28:02 -0700

    Updated

--- sw.js
+++ sw.js
@@ -1,9 +1,10 @@
 self.addEventListener("install", e => {
     e.waitUntil(
         caches.open("static").then(cache => {
-            return cache.addAll(["./", "./style.css", "./images/icons8-check-96.png",
-             "./images/icons8-todo-list-arcade-96.png", 
-             "./images/icons8-uncheck-all-96.png"]);
+            return cache.addAll(["./", "./script.js", "./index.html", "./style.css", 
+            "./images/icons8-check-96.png",
+            "./images/icons8-todo-list-arcade-96.png", 
+            "./images/icons8-uncheck-all-96.png"]);
         })
     );
 })
